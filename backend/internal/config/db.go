3a5335923c812f3757a43ff3b62e7cf631d29784
thiobista <thiobista0gedefaw@gmail.com>
2026-02-17 22:24:28 +0300

    fix render database connection

--- backend/internal/config/db.go
+++ backend/internal/config/db.go
@@ -1,11 +1,9 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -13,22 +11,16 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	godotenv.Load()
 
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+	dsn := os.Getenv("DATABASE_URL")
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		log.Fatal("❌ DB connection failed:", err)
 	}
 
 	DB = db
+
 	log.Println("✅ Database connected")
 }
